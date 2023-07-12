26d88db2aa8bc3120c1759f1762d469e701432f6
contactharrisc2 <contactharrisc2@gmail.com>
2023-07-12 02:31:16 -0500

    adds .config() - fxn that loads .env file contents to process.env

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 ///////////////////////
 // Import Dependecies
 //////////////////////
+require("dotenv").config();
+const express = require("express");
+
+
+//////////////////////
+// Server Application
+//////////////////////
+const app = express();
+const PORT = process.env.PORT;
+
+//////////////
+// app listener
+//////////////
+app.listen(PORT, ()=> console.log(`Email servicing on PORT ${PORT}`));
